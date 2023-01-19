9f1efbe96f0878aecd8db72750ca34362fc48ee8
Bruno <brunomartins1030@gmail.com>
2023-01-19 13:17:07 -0300

    add getAll method

--- src/main/java/com/bruno/cryptoapp/controller/CoinController.java
+++ src/main/java/com/bruno/cryptoapp/controller/CoinController.java
@@ -4,10 +4,7 @@ import com.bruno.cryptoapp.entity.Coin;
 import com.bruno.cryptoapp.repository.CoinRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
@@ -32,5 +29,10 @@ public class CoinController {
             return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     };
+
+    @GetMapping
+    public ResponseEntity getCoin(){
+        return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/bruno/cryptoapp/repository/CoinRepository.java
+++ src/main/java/com/bruno/cryptoapp/repository/CoinRepository.java
@@ -3,13 +3,20 @@ package com.bruno.cryptoapp.repository;
 import com.bruno.cryptoapp.entity.Coin;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 @EnableAutoConfiguration
 public class CoinRepository {
 
     private static String INSERT = "insert into coin (name, price, quantity, datetime) values (?,?,?,?)";
+
+    private static String SELECT_ALL = "select name, sum(quantity) as quantity from coin group by name";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -27,4 +34,19 @@ public class CoinRepository {
         jdbcTemplate.update(INSERT, attributes);   // aqui entao sao 2 parametros: a query sql, e os atributos para insercao
         return coin;
     }
+
+    public List<Coin> getAll(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Coin>() {    // sao 2 parametros: a query, e o RowMapper
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {   // os resultados da tabela vem no rs
+                Coin coin = new Coin();
+                coin.setName(rs.getString("name"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+
+                return coin;
+            }
+            // o map vai iterar e fazer a tarefa acima pra todas as coins que retornarem do banco de dados
+        });
+    }
+
 }
